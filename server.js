9151c3b914dfd7be9ddbb5f64f3c7eb30a03d242
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-15 00:17:10 +0530

    mongoose setup

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+require("dotenv").config();
 const app = require("./src/app.js");
+const connectDB = require("./src/config/database.js");
+
+connectDB();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
