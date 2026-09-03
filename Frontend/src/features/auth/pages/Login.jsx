fbbd609e13d37bbbd433e61f256f8974a1d4bca4
Arpita Rawat <arpitarawat381@gmail.com>
2026-09-03 21:40:31 +0530

    feat: implement login form with validation and styling

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import "../auth.form.scss"
 
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter email address"
+            />
+          </div>
 
-export default Login
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+          </div>
+            <button className = "button primary-button">Login</button>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
