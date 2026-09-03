c6d0a50004f5073321150c7ef7a7b0bfb891ec94
Arpita Rawat <arpitarawat381@gmail.com>
2026-09-03 18:27:36 +0530

    frontend setup

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -124,12 +124,11 @@ async function logoutUser(req, res) {
  * @access Private
  */
 
-
 async function getMeController(req, res) {
   const user = await userModel.findById(req.user.id);
 
   res.status(200).json({
-    message: "User fetched successfully",
+    message: "User details fetched successfully",
     user: {
       id: user._id,
       username: user.username,
