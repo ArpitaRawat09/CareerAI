de5bd49171f1c8765fb9a3c82b25f74d2910b8fb
Arpita Rawat <arpitarawat381@gmail.com>
2026-09-05 14:09:36 +0530

    feat: enhance registration form with state management and loading handling

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "../auth.form.scss";
-import {useNavigate, Link } from "react-router";
+import { useNavigate, Link } from "react-router";
+import { useAuth } from "../hooks/useAuth";
 
 const Register = () => {
-  const navigate = useNavigate()  
+  const navigate = useNavigate();
+
+  const { loading, handleRegister } = useAuth();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    // Handle form submission logic here
+    handleRegister({ username, email, password });
+    navigate("/login");
   };
+
+  if (loading) {
+    return (
+      <main>
+        <h1>Loading......</h1>
+      </main>
+    );
+  }
   return (
     <main>
       <div className="form-container">
@@ -15,7 +34,10 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="username">Username</label>
             <input
-              type="test"
+              onClick={(e) => {
+                setUsername(e.target.value);
+              }}
+              type="text"
               id="username"
               name="username"
               placeholder="Enter your Username"
@@ -25,6 +47,9 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input
+              onClick={(e) => {
+                setEmail(e.target.value);
+              }}
               type="email"
               id="email"
               name="email"
@@ -35,6 +60,9 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="password">Password</label>
             <input
+              onClick={(e) => {
+                setPassword(e.target.value);
+              }}
               type="password"
               id="password"
               name="password"
