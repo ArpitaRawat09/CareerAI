df63a956518a0b987307f4c7093e4916895adfba
Arpita Rawat <arpitarawat381@gmail.com>
2026-09-17 20:27:48 +0530

    feat: implement interview report generation with PDF parsing and AI integration

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -1,7 +1,27 @@
-async function generateInterviewReportController(){
+const pdfParse = require("pdf-parse");
+const generateInterviewReport = require("../service/ai.service.js");
 
+async function generateInterviewReportController(req, res) {
+
+  const resumeContent = pdfParse(req.file.buffer);
+  const { selfDescrption, jobDescription } = req.body;
+
+  const interReportByAi = await generateInterviewReport({
+    resume : resumeContent,
+    selfDescrption,
+    jobDescription,
+  });
+
+  const interviewReport ={
+    user : req.user._id,
+    resume : resumeContent,
+    selfDescrption,
+    jobDescription,
+    ...interReportByAi
+  }
+  res.status(201).json(interviewReport);
 }
 
 module.exports = {
-    generateInterviewReportController
-}
+  generateInterviewReportController,
+};
